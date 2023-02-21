b163ac72b70522e30d41f0c4a04e4267ba534ad1
許仁駿 <jameslovejamesjj@gmail.com>
2023-02-21 11:48:34 +0800

    加入專案檔案。

---
+++ homework002/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace homework002
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("請輸入一串以逗號分隔的整數數字字串");
+            String stringnum = Console.ReadLine();
+            List<int> list = stringnum.Split(',').Select(x => int.Parse(x)).ToList();
+            var even = list.Where(x => x % 2 == 0);
+            var odd = list.Where(x => x % 2 == 1);
+            Console.WriteLine($"奇數:{string.Join(",",odd)}");
+            Console.WriteLine($"偶數:{string.Join(",",even)}");
+            Console.ReadLine();
+        }
+    }
+}
---
+++ homeworkTue/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace homeworkTue
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {            
+            Console.WriteLine("輸入一串以逗號分隔的字串");
+            string stringnum = Console.ReadLine();
+            List<int> liststring = stringnum.Split(',').Select(x => int.Parse(x)).ToList();
+            liststring.Reverse();
+            
+            Console.WriteLine($"輸出:{string.Join(",",liststring)}");
+            Console.ReadLine();
+        }
+    }
+}
